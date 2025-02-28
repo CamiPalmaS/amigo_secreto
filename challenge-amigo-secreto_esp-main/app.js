651ee903eee72f0eb8b9dc790116c3c5a2164522
Camila Palma <capalmasoto@gmail.com>
2025-02-27 22:37:14 -0300

    establece variable amigos, y base para funcion agregarAmigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value.trimm();
+    console.log(amigo);
+
+    if (amigos.includes(amigo)){
+        document.getElementById("mensajeError").innerHTML = "El nombre ingresado ya existe.";
+    } else {
+        amigos.push(amigo);
+        console.log(amigos);
+        return amigo;
+    }
+}
