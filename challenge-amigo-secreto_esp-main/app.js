6c1408187db360d4c304d4dff68d5cc5c6d1a279
Camila Palma <capalmasoto@gmail.com>
2025-03-02 20:10:54 -0300

    se agregan validaciones y se optimizan los mensajes de error creando una funcion especifica

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,18 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let mensajeError = document.getElementById("mensajeError");
+
+function mostrarError(mensaje) {
+    mensajeError.innerHTML = mensaje;
+}
 
 function agregarAmigo() {
-    let amigo = document.getElementById("amigo").value;
-    let mensajeError = document.getElementById("mensajeError");
+    let amigo = document.getElementById("amigo").value.trim();
+    let patronTexto = /^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/;
     mensajeError.innerHTML = "";
+
     console.log(amigo);
 
-    if (amigos.includes(amigo)){
-        mensajeError.innerHTML = `El nombre ${amigo} ya existe.`;
+    //validaciones
+    if (amigo === ""){
+        return mostrarError("El campo nombre no puede estar vacio.");
+    }
+    else if (!patronTexto.test(amigo)){
+        return mostrarError("Solo puedes utilizar letras para los nombres.");
+    }
+    //
+    else if (amigos.includes(amigo)){
+        return mostrarError(`El nombre ${amigo} ya existe.`);
     } else {
         amigos.push(amigo);
         console.log(amigos);
         mostrarAmigos();
+        mostrarError("")
         return amigo;
     }
 }
@@ -28,3 +43,4 @@ function mostrarAmigos(){
         seccionLista.appendChild(elemento);
     });
 }
+
